296b24b29c20dc9499c78a3cbbb5be282a320525
Luigi Matias <igiboi26@gmail.com>
2022-11-28 20:38:57 -0500

    added login

---
+++ pages/login.tsx
@@ -0,0 +1,29 @@
+import { getProviders, signIn } from "next-auth/react";
+
+function Login({ providers }) {
+  return (
+    <div>
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" />
+
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button className="bg-[#18d860] text-white">
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
