b29255c2f89d87239f058c1a92777fde06b11bc4
Luigi Matias <igiboi26@gmail.com>
2022-12-07 13:07:17 -0500

    update provider as object with any

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,12 +2,12 @@ import { getProviders, signIn, useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
-function Login({ providers }) {
+function Login({ providers }: any) {
   return (
     <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
       <img className="w-52 mb-5" src="https://links.papareact.com/9xl" />
 
-      {Object.values(providers).map((provider) => (
+      {Object.values(providers).map((provider: any) => (
         <div key={provider.name}>
           <button
             className="bg-[#18d860] text-white p-5 rounded-lg"
